e92eb95338780dd8d1d80fcaaef5b01e34d4da01
Carl Jones <carljones9411@gmail.com>
2022-04-21 14:10:23 +0100

    created a feedback stats component and added functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
+
 import FeedbackData from "./data/feedbackData";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
+  const deleteFeedBack = (id) => {
+    if (window.confirm("Are you sure you want to delete this item?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+
+    console.log("app", id);
+  };
+
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedBack} />
       </div>
     </>
   );
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,13 +1,18 @@
+import { FaTimes } from "react-icons/fa";
+
 import PropTypes from "prop-types";
 
 import Card from "./shared/Card";
 
-const FeedbackItem = ({ item }) => {
+const FeedbackItem = ({ item, handleDelete }) => {
   const { rating, text } = item;
 
   return (
     <Card>
       <div className="num-display">{rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display">{text}</div>
     </Card>
   );
